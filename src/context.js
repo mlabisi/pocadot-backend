9b358d28fdd85db198a189ae19bc7b2aedf72949
Mora <mlabisi98@gmail.com>
2022-04-04 20:29:48 -0700

    adding Airtable service

--- src/context.js
+++
@@ -1,8 +0,0 @@
-const Airtable = require('airtable');
-const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
-  'appllL8PPXs24tCJK',
-);
-
-module.exports = {
-  Airtable: { base },
-};
---
+++ src/services/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+  Airtable: require('./Airtable'),
+}
