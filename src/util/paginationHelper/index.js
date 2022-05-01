cde9bc64a1899cf629ce6a79acafedae80810ba5
Mora <mlabisi98@gmail.com>
2022-05-01 15:32:20 -0700

    adding optional pageSize param

--- src/util/paginationHelper/index.js
+++ src/util/paginationHelper/index.js
@@ -1,13 +1,13 @@
-module.exports.getPage = async (page, dataSource) => {
+module.exports.getPage = async (page, dataSource, pageSize = 100) => {
   const records = await dataSource.getAll();
-  const pagesCt = Math.ceil(records.length / 100);
+  const pagesCt = Math.ceil(records.length / pageSize);
 
   if (page > pagesCt) {
     throw new Error('The provided page number was invalid.');
   }
 
-  const firstIndex = ((page - 1) * 100) === 0 ? 0 : ((page - 1) * 100) + 1;
-  const lastIndex = ((page * 100) >= records.length ? (records.length) - 1 : (page * 100)) + 1;
+  const firstIndex = ((page - 1) * pageSize) === 0 ? 0 : ((page - 1) * pageSize) + 1;
+  const lastIndex = ((page * pageSize) >= records.length ? (records.length) - 1 : (page * pageSize)) + 1;
 
   return records.slice(firstIndex, lastIndex);
 }
