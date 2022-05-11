0ff275a80c3ece5a2c7f28d075ad6366f2587d87
Mora <mlabisi98@gmail.com>
2022-05-11 11:37:46 -0700

    adding operations for talent

--- src/resolvers/talent.resolver.js
+++ src/resolvers/talent.resolver.js
@@ -11,13 +11,30 @@ module.exports = {
     }
   },
   Query: {
-    preferencesFeed: async (root, __, { dataSources }) => {
+    preferencesFeed: async (_, __, { dataSources }) => {
       const groups = await filter(dataSources.groups);
       const idols = await filter(dataSources.idols, {
         fields: { isSolo: true },
       });
 
       return [...groups, ...idols]
+    },
+    userPreferences: async (_, { input }, { dataSources }) => {
+      const { ids } = input
+      const faveGroups = await dataSources.users.getFaveGroups(ids[0])
+      const faveIdols = await dataSources.users.getFaveIdols(ids[0])
+
+      return [...faveGroups, ...faveIdols]
     }
+
   },
+  Mutation: {
+    userPreferences: async (_, { input }, { dataSources }) => {
+      const updated = await dataSources.users.update([input]).then((updated) => updated[0]);
+      const faveGroups = await dataSources.users.getFaveGroups(updated.id)
+      const faveIdols = await dataSources.users.getFaveIdols(updated.id)
+
+      return [...faveGroups, ...faveIdols]
+    },
+  }
 };
