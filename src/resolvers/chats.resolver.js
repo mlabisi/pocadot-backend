9e2c920caedfedd1883f1cbc1d552df00611ab2b
Mora <mlabisi98@gmail.com>
2022-09-15 18:19:05 -0700

    implementing chat schema

--- src/resolvers/chats.resolver.js
+++ src/resolvers/chats.resolver.js
@@ -13,7 +13,7 @@ module.exports = {
   },
   Chat: {
     id: (conversation) => conversation.sid,
-    date: (conversation) => conversation.dateCreated.toString(),
+    timestamp: (conversation) => conversation.dateCreated.toString(),
     participants: async (conversation, __, context) => {
       return (
         await context.dataSources.chats.fetchParticipants(conversation.sid)
@@ -34,6 +34,6 @@ module.exports = {
     id: (message) => message.sid,
     author: (message, __, context) =>
       context.dataSources.users.getById(message.author),
-    date: (message) => message.date_created,
+    timestamp: (message) => message.date_created,
   },
 };
